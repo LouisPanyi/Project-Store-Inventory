2bb53890a3c2dfd881979c1637e1a0ad1def2159
LouisPanyi <105207837+LouisPanyi@users.noreply.github.com>
2025-08-23 04:20:33 +0700

    Adding Product Image For Homepage

---
+++ app/ui/produklist.tsx
@@ -0,0 +1,19 @@
+
+export const produkList = [
+  {
+    name: "Rosario Kayu Zaitun",
+    image: "/produk/rosario.jpg",
+  },
+  {
+    name: "Patung Bunda Maria",
+    image: "/produk/patung-bunda-maria.jpeg",
+  },
+  {
+    name: "Salib Kayu Ukir",
+    image: "/produk/salib.jpg",
+  },
+  {
+    name: "Buku Doa Adalah Sumber Kekuatan",
+    image: "/produk/buku-doa-adalah-sumber-kekuatan.jpg",
+  },
+];
