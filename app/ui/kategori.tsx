b86b2d35ffdd67e2a88071265378a6a0cf7d6e2a
LouisPanyi <105207837+LouisPanyi@users.noreply.github.com>
2025-08-29 22:27:47 +0700

    adding missing export const

---
+++ app/ui/kategori.tsx
@@ -0,0 +1,18 @@
+export const kategori = [
+    {
+        name: "Perlengkapan Misa",
+        image: "/produk/a.misa.jpg"
+    },
+    {
+        name: "Buku & Doa",
+        image: "/produk/b.doa.png"
+    },
+    {
+        name: "Rosario",
+        image: "/produk/e.rosario.jpg"
+    },
+    {
+        name: "Patung & Salib",
+        image: "/produk/p.salib.jpeg"
+    },
+];
